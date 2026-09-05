e67b5045a3a305471983c9065072519900bea54e
sowjitherode <sowjitherode@gmail.com>
2026-09-05 12:04:46 +0530

    stepweek4hw1.java

---
+++ stepweek4hw1.java
@@ -0,0 +1,38 @@
+class Participant {
+    String name;
+    String teamName;
+    boolean registered;
+
+    public Participant(String name, String teamName) {
+        this.name = name;
+        this.teamName = teamName;
+        this.registered = true;
+    }
+
+    public Participant(String name) {
+        this(name, "Unassigned");
+    }
+
+    public void printStatus() {
+        System.out.println(name + " | " + teamName + " | Registered: " + registered);
+    }
+}
+
+public class HackathonRegistration {
+    public static void main(String[] args) {
+        String[] names = {"Ravi", "Meera", "Karthik", "Divya"};
+        String[] teamNames = {"ByteBusters", "", "CodeCrafters", ""};
+
+        for (int i = 0; i < names.length; i++) {
+            Participant participant;
+
+            if (teamNames[i].equals("")) {
+                participant = new Participant(names[i]);
+            } else {
+                participant = new Participant(names[i], teamNames[i]);
+            }
+
+            participant.printStatus();
+        }
+    }
+}
