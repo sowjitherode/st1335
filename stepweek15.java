d19bcd243e855de0478726de107242c08863eef8
sowjitherode <sowjitherode@gmail.com>
2026-09-05 11:17:33 +0530

    stepweek15.java

---
+++ stepweek15.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class ReverseCustomerName {
+    static String reverseCustomerName(String customerName) {
+        char[] characters = customerName.toCharArray();
+
+        int i = 0, j = characters.length - 1;
+
+        while (i < j) {
+            char temp = characters[i];
+            characters[i] = characters[j];
+            characters[j] = temp;
+
+            i++;
+            j--;
+        }
+
+        return new String(characters);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter customer name: ");
+        String customerName = sc.nextLine();
+
+        String reversedName = reverseCustomerName(customerName);
+
+        System.out.println("Original Name: " + customerName);
+        System.out.println("Reversed Name: " + reversedName);
+    }
+}
