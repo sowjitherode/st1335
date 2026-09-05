cd5301b50c0d1c9201eae85d46a5317973a8eaf9
sowjitherode <sowjitherode@gmail.com>
2026-09-05 12:08:13 +0530

    stepweek4hw4.java

---
+++ stepweek4hw4.java
@@ -0,0 +1,36 @@
+class MembershipCard {
+    static String libraryName;
+    static String validUntil;
+    String studentName;
+
+    static {
+        libraryName = "SRM Central Library";
+        validUntil = "May 2027";
+        System.out.println("Library info loaded");
+    }
+
+    public MembershipCard(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public void printConfirmation() {
+        System.out.println("Membership card issued: " + studentName);
+    }
+}
+
+public class LibraryMembership {
+    public static void main(String[] args) {
+        String[] names = {
+            "Ananya",
+            "Rohan",
+            "Priya",
+            "Arjun",
+            "Sneha"
+        };
+
+        for (String name : names) {
+            MembershipCard card = new MembershipCard(name);
+            card.printConfirmation();
+        }
+    }
+}
