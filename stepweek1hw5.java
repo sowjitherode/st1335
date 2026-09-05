14b5cf679254fc6ae1e1f6ccbd604c1b05d81265
sowjitherode <sowjitherode@gmail.com>
2026-09-05 11:27:12 +0530

    stepweek1hw5.java

---
+++ stepweek1hw5.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class MovieReviewProfiler {
+    static void classifyWordLengths(String review) {
+        String[] words = review.trim().split("\\s+");
+
+        int shortWords = 0;
+        int mediumWords = 0;
+        int longWords = 0;
+
+        for (String word : words) {
+            int length = word.length();
+
+            if (length >= 1 && length <= 4)
+                shortWords++;
+            else if (length <= 8)
+                mediumWords++;
+            else
+                longWords++;
+        }
+
+        System.out.println("Short: " + shortWords);
+        System.out.println("Medium: " + mediumWords);
+        System.out.println("Long: " + longWords);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter movie review: ");
+        String review = sc.nextLine();
+
+        classifyWordLengths(review);
+    }
+}
